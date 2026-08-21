b518bea7ffff6a5da04680e849ceb337af13063c
pSujalp <sujalpdevarathnam@gmail.com>
2026-08-21 21:05:39 +0530

    Camera Class

--- Main.cs
+++ Main.cs
@@ -3,6 +3,7 @@ using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using System.Numerics;
 using Silk.NET.Maths;
+using FreeCam;
 
 
 namespace Tutorial
@@ -73,14 +74,7 @@ namespace Tutorial
         private static readonly uint[] Indices ={0, 1, 2};
 
 
-        private static Vector3 CameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
-        private static Vector3 CameraFront = new Vector3(0.0f, 0.0f, -1.0f);
-        private static Vector3 CameraUp = Vector3.UnitY;
-        private static Vector3 CameraDirection = Vector3.Zero;
-        private static float CameraYaw = -90f;
-        private static float CameraPitch = 0f;
-        private static float CameraZoom = 45f;
-        private static Vector2 LastMousePosition;
+        private static Camera camera ;
 
 
         private static void Main(string[] args)
@@ -93,10 +87,12 @@ namespace Tutorial
             window = Window.Create(options);
             window.Load += OnLoad;
             window.Render += OnRender;
+            window.Update += OnUpdate;
             window.FramebufferResize += OnFramebufferResize;
             window.Closing += OnClose;
             window.Run();
             window.Dispose();
+
         }
 
 
@@ -108,16 +104,10 @@ namespace Tutorial
             if (primaryKeyboard != null)
             {
                 primaryKeyboard.KeyDown += KeyDown;
+                camera = new Camera(primaryKeyboard,input);
             }
-            for (int i = 0; i < input.Mice.Count; i++)
-            {
-                input.Mice[i].Cursor.CursorMode = CursorMode.Raw;
-                input.Mice[i].MouseMove += OnMouseMove;
-                input.Mice[i].Scroll += OnMouseWheel;
-            }
+
             Gl = GL.GetApi(window);
-
-
             Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
             Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
             Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
@@ -125,7 +115,35 @@ namespace Tutorial
             Vao.VertexAttributePointer(1, 2, VertexAttribPointerType.Float, 5, 3);
             Shader = new Shader(Gl, "shaders/shader.vert", "shaders/shader.frag");
             Texture = new Texture(Gl,"assets/Grass_Block_TEX1.png");
+
             
+            
+        }
+
+        private static unsafe void OnUpdate(double deltaTime)
+        {
+            var moveSpeed = 2.5f * (float)deltaTime;
+
+            if (primaryKeyboard.IsKeyPressed(Key.W))
+            {
+                //Move forwards
+                Camera.CameraPosition += moveSpeed * Camera.CameraFront;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.S))
+            {
+                //Move backwards
+                Camera.CameraPosition -= moveSpeed * Camera.CameraFront;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.A))
+            {
+                //Move left
+                Camera.CameraPosition -= Vector3.Normalize(Vector3.Cross(Camera.CameraFront, Camera.CameraUp)) * moveSpeed;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.D))
+            {
+                //Move right
+                Camera.CameraPosition += Vector3.Normalize(Vector3.Cross(Camera.CameraFront, Camera.CameraUp)) * moveSpeed;
+            }
         }
 
         private static unsafe void OnRender(double obj)
@@ -143,8 +161,8 @@ namespace Tutorial
 
 
             var model = Matrix4x4.CreateRotationY(MathHelper.DegreesToRadians(difference)) * Matrix4x4.CreateRotationX(MathHelper.DegreesToRadians(difference));
-            var view = Matrix4x4.CreateLookAt(CameraPosition, CameraPosition + CameraFront, CameraUp);
-            var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(CameraZoom), (float)size.X / size.Y, 0.1f, 100.0f);
+            var view = Matrix4x4.CreateLookAt(Camera.CameraPosition, Camera.CameraPosition + Camera.CameraFront, Camera.CameraUp);
+            var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(Camera.CameraZoom), (float)size.X / size.Y, 0.1f, 100.0f);
 
             Shader.SetUniform("uModel", model);
             Shader.SetUniform("uView", view);
@@ -168,34 +186,9 @@ namespace Tutorial
             Texture.Dispose();
         }
 
-        private static unsafe void OnMouseMove(IMouse mouse, Vector2 position)
-        {
-            var lookSensitivity = 0.1f;
-            if (LastMousePosition == default) { LastMousePosition = position; }
-            else
-            {
-                var xOffset = (position.X - LastMousePosition.X) * lookSensitivity;
-                var yOffset = (position.Y - LastMousePosition.Y) * lookSensitivity;
-                LastMousePosition = position;
+       
 
-                CameraYaw += xOffset;
-                CameraPitch -= yOffset;
-
-                //We don't want to be able to look behind us by going over our head or under our feet so make sure it stays within these bounds
-                CameraPitch = Math.Clamp(CameraPitch, -89.0f, 89.0f);
-
-                CameraDirection.X = MathF.Cos(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
-                CameraDirection.Y = MathF.Sin(MathHelper.DegreesToRadians(CameraPitch));
-                CameraDirection.Z = MathF.Sin(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
-                CameraFront = Vector3.Normalize(CameraDirection);
-            }
-        }
-
-        private static unsafe void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel)
-        {
-            //We don't want to be able to zoom in too close or too far away so clamp to these values
-            CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
-        }
+       
 
 
         private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
@@ -205,5 +198,6 @@ namespace Tutorial
                 window.Close();
             }
         }
+       
     }
 }
---
+++ src/Camera.cs
@@ -0,0 +1,78 @@
+using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System.Numerics;
+using Silk.NET.Maths;
+using Tutorial;
+
+
+
+
+
+namespace FreeCam
+{
+
+    class Camera
+    {
+        public Camera() { }
+
+        public Camera(IKeyboard primaryKeyboard1, IInputContext input)
+        {
+            primaryKeyboard = primaryKeyboard1;
+            for (int i = 0; i < input.Mice.Count; i++)
+            {
+                input.Mice[i].Cursor.CursorMode = CursorMode.Raw;
+                input.Mice[i].MouseMove += OnMouseMove;
+                input.Mice[i].Scroll += OnMouseWheel;
+            }
+            
+        }
+
+        private static IKeyboard primaryKeyboard;
+        public static Vector3 CameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
+        public static Vector3 CameraFront = new Vector3(0.0f, 0.0f, -1.0f);
+        public static Vector3 CameraUp = Vector3.UnitY;
+        public static Vector3 CameraDirection = Vector3.Zero;
+        public static float CameraYaw = -90f;
+        public static float CameraPitch = 0f;
+        public static float CameraZoom = 45f;
+        private static Vector2 LastMousePosition;
+
+
+        private static unsafe void OnMouseMove(IMouse mouse, Vector2 position)
+        {
+            var lookSensitivity = 0.1f;
+            if (LastMousePosition == default) { LastMousePosition = position; }
+            else
+            {
+                var xOffset = (position.X - LastMousePosition.X) * lookSensitivity;
+                var yOffset = (position.Y - LastMousePosition.Y) * lookSensitivity;
+                LastMousePosition = position;
+
+                CameraYaw += xOffset;
+                CameraPitch -= yOffset;
+
+                //We don't want to be able to look behind us by going over our head or under our feet so make sure it stays within these bounds
+                CameraPitch = Math.Clamp(CameraPitch, -89.0f, 89.0f);
+
+                CameraDirection.X = MathF.Cos(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Y = MathF.Sin(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Z = MathF.Sin(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraFront = Vector3.Normalize(CameraDirection);
+            }
+        }
+
+        private static unsafe void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel)
+        {
+            //We don't want to be able to zoom in too close or too far away so clamp to these values
+            CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
+        }
+
+
+        
+
+
+    }
+
+
+}
